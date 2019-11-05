48951e40c684239d7d7fc8a6d3675a5e41bf4af7
prokawsar <prokawsar@gmail.com>
2019-11-05 21:39:02 +0600

    added clear icon on search bar

--- src/components/views/SearchBar.svelte
+++ src/components/views/SearchBar.svelte
@@ -4,9 +4,15 @@ import LinkViewer from './ResultPanel.svelte';
 
 export let result_state = false
 
+let youtube_link = ''
+
 let getLink = (event) => {
   event.stopPropagation()
 	result_state = true
+}
+
+let clear = (event) => {
+	youtube_link = ''
 }
 </script>
 
@@ -14,11 +20,13 @@ let getLink = (event) => {
 	<h1 class="title has-text-centered">YouTube video downloader</h1>
 
 	<div class="field">
-		<div class="control has-text-centered">
-			<input class="input is-info" type="text" placeholder="YouTube link">
-			<span class="icon is-small is-right">
-				<i class="fa fa-check"></i>
-			</span>
+		<div class="control has-icons-right">
+			<input class="input is-info" type="text" bind:value={youtube_link} placeholder="YouTube link">
+			{#if youtube_link != ''}
+				<span class="icon is-small is-right" on:click={clear}>
+					<i class="fas fa-times-circle" style="cursor: pointer;"></i>
+				</span>
+			{/if}
 		</div>
 	</div>
 	<div class="field">
