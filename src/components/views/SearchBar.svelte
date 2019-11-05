4bf640b93eb8b1c8f25f119ff5d5f098d39adbe8
prokawsar <prokawsar@gmail.com>
2019-11-05 20:55:34 +0600

    added button event

--- src/components/views/SearchBar.svelte
+++ src/components/views/SearchBar.svelte
@@ -1,12 +1,25 @@
+<script>
 
+import LinkViewer from './ResultPanel.svelte';
+
+let result_state = false
+
+let getLink = (event) => {
+  event.stopPropagation()
+	result_state = true
+}
+</script>
 <div>
 	<h1 class="title has-text-centered">YouTube video downloader</h1>
 
 	<div class="field">
-		<div class="control">
+		<div class="control has-text-centered">
 			<input class="input is-info" type="text" placeholder="YouTube link">
-			<a class="button is-link" href="#">Get Link</a>
+			<button class="button is-link" on:click={getLink}>Get Link</button>
 		</div>
 	</div>
 
+	{#if result_state}
+		<LinkViewer />
+	{/if}
 </div>
