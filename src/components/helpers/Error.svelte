1c9470ba4890448c180553620601c588fa86fb40
prokawsar <prokawsar@gmail.com>
2019-11-06 20:00:39 +0600

    add error message model

---
+++ src/components/helpers/Error.svelte
@@ -0,0 +1,21 @@
+	<script>
+    export let error = false
+  </script>
+
+  <div class="modal {error ? 'is-active' : ''}">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box is-danger">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>Invalid YouTube link ! Ex. https://www.youtube.com/watch?v=abcdefg</strong>
+                <br>
+                  Also be informed that, currently supports only single video link, not playlist.
+              </p>
+            </div>
+          </div>
+      </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" on:click={() => {error = !error}}></button>
+  </div>
